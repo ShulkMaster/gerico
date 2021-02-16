b1398425e117a5958994d8c6a45ca69954b62f94
Yury Rivera <yrivera@applaudostudios.com>
2021-02-15 21:06:27 -0600

    :sparkles: filter function with generics

--- src/arrays.ts
+++ src/arrays.ts
@@ -24,3 +24,49 @@ const data: user[] = [
         name: 'Luis'
     }
 ];
+
+const filterBasic = (array: number[], predicate: (number: number) => boolean): number[] => {
+    const result: number[] = [];
+    for (const element of array) {
+        if(predicate(element)){
+            result.push(element);
+        }
+    }
+    return result;
+};
+
+const filterBasic2 = (array: string[], predicate: (number: string) => boolean): string[] => {
+    const result: string[] = [];
+    for (const element of array) {
+        if(predicate(element)){
+            result.push(element);
+        }
+    }
+    return result;
+};
+
+const filterBasic3 = (array: any[], predicate: (number: any) => boolean): any[] => {
+    const result: string[] = [];
+    for (const element of array) {
+        if(predicate(element)){
+            result.push(element);
+        }
+    }
+    return result;
+};
+
+const filter = <T>(array: T[], predicate: (e: T) => boolean): T[] => {
+    const result: T[] = [];
+    for (const element of array) {
+        if(predicate(element)){
+            result.push(element);
+        }
+    }
+    return result;
+};
+
+const res = filterBasic(numbers, n => n % 2 === 0);
+console.log(res);
+
+const resGeneric = filter(data, e => e.age > 18);
+console.log(res);
