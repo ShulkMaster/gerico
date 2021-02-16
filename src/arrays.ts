8ae579af118f146c3f9775e831b6ced93ace17d1
Yury Rivera <yrivera@applaudostudios.com>
2021-02-15 20:36:24 -0600

    firts commit :tada:

---
+++ src/arrays.ts
@@ -0,0 +1,26 @@
+const numbers: number[] = [1, 2, 3, 4, 5];
+const text: string[] = ['mucho', 'texto', 'mucha', 'compu'];
+
+type user = {
+    id: number,
+    name: string,
+    age: number,
+}
+
+const data: user[] = [
+    {
+        age: 15,
+        id: 10,
+        name: 'Juanito'
+    },
+    {
+        age: 19,
+        id: 11,
+        name: 'Pedro'
+    },
+    {
+        age: 25,
+        id: 12,
+        name: 'Luis'
+    }
+];
