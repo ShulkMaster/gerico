e957edce18c2ec01cc05a031aca524e10b63a8e4
Yury Rivera <yrivera@applaudostudios.com>
2021-02-16 01:38:01 -0600

    :sparkles: example 4 with react

---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import {example1} from './arrays';
+import './styles.css';
+
+export const App = () => {
+  const [state, setState] = useState('red');
+  return (
+    <div>
+      <button onClick={() => setState('red')}>Red</button>
+      <button onClick={() => setState('green')}>Green</button>
+      <button onClick={() => setState('blue')}>Blue</button>
+      <SimpleWrapper text={state} className={state} onClick={example1}/>
+    </div>
+  );
+}
+
+type DivProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+const SimpleWrapper = ({text, ...props}: { text: string } & DivProps) => {
+  return (
+    <div {...props}>
+      <p>{text} a</p>
+    </div>
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import ReactDOM from 'react-dom';
 import {example1} from './arrays';
 import {example2} from './composition';
 import {example3} from './data';
+import {App} from './app';
 
 //example1();
 //example2();
-example3()
+//example3();
+
+ReactDOM.render(App(), document.getElementById('root'));
