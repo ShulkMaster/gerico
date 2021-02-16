de0e259cd9655f54e4a433a066a8b50070c10bc4
Yury Rivera <yrivera@applaudostudios.com>
2021-02-15 21:34:08 -0600

    :hammer: setting up webpack

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,31 +1,4 @@
-const numbers: number[] = [1, 2, 3, 4, 5];
-const text: string[] = ['mucho', 'texto', 'mucha', 'compu'];
-
-type user = {
-    id: number,
-    name: string,
-    age: number,
-}
-
-const data: user[] = [
-    {
-        age: 15,
-        id: 10,
-        name: 'Juanito'
-    },
-    {
-        age: 19,
-        id: 11,
-        name: 'Pedro'
-    },
-    {
-        age: 25,
-        id: 12,
-        name: 'Luis'
-    }
-];
-
-const filterBasic = (array: number[], predicate: (number: number) => boolean): number[] => {
+export const filterBasic = (array: number[], predicate: (number: number) => boolean): number[] => {
     const result: number[] = [];
     for (const element of array) {
         if(predicate(element)){
@@ -35,7 +8,7 @@ const filterBasic = (array: number[], predicate: (number: number) => boolean): n
     return result;
 };
 
-const filterBasic2 = (array: string[], predicate: (number: string) => boolean): string[] => {
+export const filterBasic2 = (array: string[], predicate: (number: string) => boolean): string[] => {
     const result: string[] = [];
     for (const element of array) {
         if(predicate(element)){
@@ -45,7 +18,7 @@ const filterBasic2 = (array: string[], predicate: (number: string) => boolean): 
     return result;
 };
 
-const filterBasic3 = (array: any[], predicate: (number: any) => boolean): any[] => {
+export const filterBasic3 = (array: any[], predicate: (number: any) => boolean): any[] => {
     const result: string[] = [];
     for (const element of array) {
         if(predicate(element)){
@@ -55,7 +28,7 @@ const filterBasic3 = (array: any[], predicate: (number: any) => boolean): any[] 
     return result;
 };
 
-const filter = <T>(array: T[], predicate: (e: T) => boolean): T[] => {
+export const filter = <T>(array: T[], predicate: (e: T) => boolean): T[] => {
     const result: T[] = [];
     for (const element of array) {
         if(predicate(element)){
@@ -64,9 +37,3 @@ const filter = <T>(array: T[], predicate: (e: T) => boolean): T[] => {
     }
     return result;
 };
-
-const res = filterBasic(numbers, n => n % 2 === 0);
-console.log(res);
-
-const resGeneric = filter(data, e => e.age > 18);
-console.log(res);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 4200,
+  },
+};
